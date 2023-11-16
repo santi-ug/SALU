8a3fef04e51a3b2d64602d35fe30792f230600c9
vexvo <santiago.uribeg@autonoma.edu.co>
2023-11-16 16:39:14 -0500

    agregar importacion de modelo a cada controlador

--- server/app.js
+++ server/app.js
@@ -12,19 +12,3 @@ app.use(cors());
 app.use('/api/v1', mainRouter);
 
 export default app;
-
-// const User = require('./user.model');
-// // Create a new user
-// const newUser = new User({
-//  username: 'john_doe',
-//  email: 'john@example.com',
-//  password: 'password123',
-// });
-// // Save the user to the database
-// newUser.save()
-//  .then((user) => {
-//  console.log('User created:', user);
-//  })
-//  .catch((error) => {
-//  console.error('Error creating user:', error);
-//  });
---
+++ server/src/controllers/reviewController.js
@@ -0,0 +1,1 @@
+import review from "../models/review.js";
