ab3dd0045a7df993e490381bdbd94d01bb5a2d61
vexvo <santiago.uribeg@autonoma.edu.co>
2023-11-16 17:29:49 -0500

    conexion base de datos funcional

--- server/app.js
+++ server/app.js
@@ -1,30 +1,14 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+import db from "./src/config/dbconfig.js";
 import mainRouter from "./src/routes/mainRouter.js";
 const app = express();
 
 app.use(cors());
-// mongoose.connect('mongodb://localhost/DATABASENAME', { useNewUrlParser: true, useUnifiedTopology: true});
-
+db();
 
 
 app.use('/api/v1', mainRouter);
 
 export default app;
-
-// const User = require('./user.model');
-// // Create a new user
-// const newUser = new User({
-//  username: 'john_doe',
-//  email: 'john@example.com',
-//  password: 'password123',
-// });
-// // Save the user to the database
-// newUser.save()
-//  .then((user) => {
-//  console.log('User created:', user);
-//  })
-//  .catch((error) => {
-//  console.error('Error creating user:', error);
-//  });
