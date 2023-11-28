8f17b7866cf4440bc0d791f9d064f4cc098098f0
vexvo <santiago.uribeg@autonoma.edu.co>
2023-11-28 16:51:26 -0500

    fixed next not running

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
                     {/* mobile button goes here */}
                     <div className="md:hidden flex items-center" onClick={ () => setToggleHamburgerMenu(!toggleHamburgerMenu)}>
                         <button className="mobile-menu-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-9 h-9">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>  
                         </button>
